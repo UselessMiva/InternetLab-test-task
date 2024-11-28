cbc7c8ed0c09d206e1c3f8638b2fa20270a37e4b
H0r0sho <undeaddragon163rus@gmail.com>
2024-11-28 18:14:27 +0400

    fix

--- src/app/about/dataAbout.service.ts
+++ src/app/about/dataAbout.service.ts
@@ -15,27 +15,27 @@ export class DataAboutService {
       id: 1,
       title: 'Прочитай задание внимательно',
       subtitle: 'Думаю у тебя не займет это больше двух-трех минут',
-      img: '../../assets/images/waiting.svg',
+      img: 'assets/images/waiting.svg',
     },
     {
       id: 2,
       title: 'Изучи весь макет заранее',
       subtitle:
         'Подумай как это будет работать на разных разрешениях и при скролле',
-      img: '../../assets/images/delivery-truck.svg',
+      img: 'assets/images/delivery-truck.svg',
     },
     {
       id: 3,
       title: 'Сделай хорошо',
       subtitle: 'Чтобы мы могли тебе доверить подобные задачи в будущем',
-      img: '../../assets/images/secure.svg',
+      img: 'assets/images/secure.svg',
     },
     {
       id: 4,
       title: 'Получи предложение',
       subtitle:
         'Ну тут все просто, не я придумал правила, но думаю так и будет)))',
-      img: '../../assets/images/money-bags.svg',
+      img: 'assets/images/money-bags.svg',
     },
   ];
 
--- src/app/head/head.component.ts
+++ src/app/head/head.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-head',
   templateUrl: './head.component.html',
+  standalone: true,
   imports: [HeaderComponent],
   styleUrls: ['./head.component.css'],
 })
-export class HeadComponent {}
+export class HeadComponent {
+  constructor(private snackBar: MatSnackBar) {}
+  onSubmit(event: Event): void {
+    this.snackBar.open('Кнопки нажата!', 'Закрыть', {
+      duration: 3000,
+    });
+  }
+}
